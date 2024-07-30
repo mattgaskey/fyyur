f022f3eba4bff13f100824009ff2aff2c6fbab90
Matt Gaskey <mattgaskey@gmail.com>
2024-07-29 19:27:35 -0700

    improved show form

--- fyyur/show/forms.py
+++ fyyur/show/forms.py
@@ -1,34 +1,32 @@
 from flask import render_template, flash
-from datetime import datetime
+from datetime import datetime, timezone
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateTimeField
-from wtforms.validators import DataRequired, ValidationError
+from wtforms import SelectField, DateTimeLocalField
+from wtforms.validators import DataRequired
 import sqlalchemy as sa
 from fyyur import db
 from fyyur.models import Artist, Venue
 
 class ShowForm(FlaskForm):
-    artist_id = StringField(
+    artist_id = SelectField(
         'artist_id',
-        validators=[DataRequired()]
-    )
-    venue_id = StringField(
-        'venue_id',
-        validators=[DataRequired()]
-    )
-    start_time = DateTimeField(
-        'start_time',
-        format='%Y-%m-%d %I:%M %p',
         validators=[DataRequired()],
-        default=datetime.today()
+        choices=[]
+    )
+    venue_id = SelectField(
+        'venue_id',
+        validators=[DataRequired()],
+        choices=[]
+    )
+    start_time = DateTimeLocalField(
+        'start_time',
+        validators=[DataRequired()],
+        default=datetime.now(timezone.utc)
     )
 
-    def validate_artist_id(self, artist_id):
-      artist = db.session.scalar(sa.select(Artist).where(Artist.id == artist_id.data))
-      if not artist:
-        raise ValidationError('Artist does not exist.')
-    
-    def validate_venue_id(self, venue_id):
-      venue = db.session.scalar(sa.select(Venue).where(Venue.id == venue_id.data))
-      if not venue:
-        raise ValidationError('Venue does not exist.')
+    def __init__(self, *args, **kwargs):
+      super(ShowForm, self).__init__(*args, **kwargs)
+      artists = db.session.scalars(sa.select(Artist).order_by(Artist.name)).all()
+      self.artist_id.choices = [(artist.id, artist.name) for artist in artists] if artists else [('', 'No artist available')]
+      venues = db.session.scalars(sa.select(Venue).order_by(Venue.name)).all()
+      self.venue_id.choices = [(venue.id, f"{venue.name} - {venue.city}, {venue.state}") for venue in venues] if venues else [('', 'No venue available')]
